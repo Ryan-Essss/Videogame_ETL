dd4928623dee70e642ef2e964b914fdab1fe1094
Ryan S <sabon.ryan@gmail.com>
2021-03-24 22:08:36 -0400

    added schema

--- vg_sales_schema.sql
+++ vg_sales_schema.sql
@@ -1,4 +1,14 @@
-DROP TABLE vg_sales_clean;
+Select * FROM vg_sales_clean;
+SELECT * FROM developer;
+SELECT * FROM platform;
+SELECT * FROM publisher;
+
+
+DROP TABLE IF EXISTS vg_sales_clean;
+DROP TABLE IF EXISTS developer;
+DROP TABLE IF EXISTS platform; 
+DROP TABLE IF EXISTS publisher; 
+
 
 CREATE TABLE vg_sales_clean (
 rank INT NOT NULL,
@@ -14,4 +24,31 @@ other_sales DEC,
 global_sales DEC
 );
 
-Select * from vg_sales_clean;
+CREATE TABLE developer (
+    developer VARCHAR,
+    dev_country VARCHAR,
+    dev_est INT
+);
+
+CREATE TABLE platform (
+    platform VARCHAR,
+    pform_full VARCHAR,
+    pform_manuf VARCHAR
+);
+
+CREATE TABLE publisher (
+    publisher VARCHAR,
+    pub_est INT
+);
+
+
+Select * FROM vg_sales_clean;
+SELECT * FROM developer;
+SELECT * FROM platform;
+SELECT * FROM publisher;
+
+
+DROP TABLE IF EXISTS vg_sales_clean;
+DROP TABLE IF EXISTS developer;
+DROP TABLE IF EXISTS platform; 
+DROP TABLE IF EXISTS publisher; 
