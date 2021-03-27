34ea96b6bb06809fcfe425695b7917110e22ac5c
Ryan S <sabon.ryan@gmail.com>
2021-03-27 11:54:04 -0400

    sql alchemy and joins complete

--- vg_sales_schema.sql
+++ vg_sales_schema.sql
@@ -1,18 +1,16 @@
 Select * FROM vg_sales_clean;
-SELECT * FROM developer;
 SELECT * FROM platform;
 SELECT * FROM publisher;
 
 
 DROP TABLE IF EXISTS vg_sales_clean;
-DROP TABLE IF EXISTS developer;
 DROP TABLE IF EXISTS platform; 
 DROP TABLE IF EXISTS publisher; 
 
 
 CREATE TABLE vg_sales_clean (
-rank INT NOT NULL,
-name VARCHAR,
+rank INT,
+name VARCHAR PRIMARY KEY,
 platform VARCHAR,
 year INT,
 genre VARCHAR,
@@ -24,31 +22,35 @@ other_sales DEC,
 global_sales DEC
 );
 
-CREATE TABLE developer (
-    developer VARCHAR,
-    dev_country VARCHAR,
-    dev_est INT
-);
 
 CREATE TABLE platform (
     platform VARCHAR,
-    pform_full VARCHAR,
-    pform_manuf VARCHAR
+    full_platform VARCHAR,
+    platform_manufacturer VARCHAR
 );
 
 CREATE TABLE publisher (
     publisher VARCHAR,
+	pub_hq VARCHAR,
     pub_est INT
 );
 
 
 Select * FROM vg_sales_clean;
-SELECT * FROM developer;
 SELECT * FROM platform;
 SELECT * FROM publisher;
 
 
 DROP TABLE IF EXISTS vg_sales_clean;
-DROP TABLE IF EXISTS developer;
 DROP TABLE IF EXISTS platform; 
 DROP TABLE IF EXISTS publisher; 
+
+
+-- Joins tables
+SELECT vg_sales_clean.*, platform.full_platform, platform.platform_manufacturer, publisher.pub_hq, publisher.pub_est
+FROM vg_sales_clean
+LEFT JOIN platform
+ON vg_sales_clean.platform = platform.platform
+LEFT JOIN publisher
+ON vg_sales_clean.publisher = publisher.publisher
+
